94783ba2a50f97cff8163b0b107f4e9fd380b55c
Skeletonxf <skeletonxf@gmail.com>
2019-01-05 17:13:48 +0000

    Experiment with opacity on annotations

--- addon/build/sidebar.js
+++ addon/build/sidebar.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://github.com/Khan/tota11y/blob/master/LICENSE.txt
  * 
- * Date: 2019-01-03
+ * Date: 2019-01-05
  * 
  */
 /******/ (function(modules) { // webpackBootstrap
