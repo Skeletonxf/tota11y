9b14d16b12398f7f822d192bbfd16a9efe9efb8d
Skeletonxf <skeletonxf@gmail.com>
2019-03-08 10:20:57 +0000

    Improve comments on CI configs

--- sync.sh
+++ sync.sh
@@ -10,7 +10,7 @@ git checkout master
 ## https://gitlab.com/gitlab-examples/ssh-private-key/issues/1#note_48526556
 echo "$SSH_GITHUB_PUSH_SYNC" | tr -d '\r' | ssh-add - > /dev/null
 
-# now add the remotes for our private repositories using ssh
+# now add the remotes for the GitHub repo using SSH
 git remote add github git@github.com:Skeletonxf/totally-automated-a11y-scanner.git
-# Push onto the private repository
+# Push onto GitHub
 git push github master
