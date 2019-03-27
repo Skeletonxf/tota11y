d3f36d6697ccb2d0a789385bfbe84aeecb32af22
Skeletonxf <skeletonxf@gmail.com>
2019-03-27 17:08:54 +0000

    Move adding SSH key into main CI script

--- sync.sh
+++ sync.sh
@@ -4,12 +4,6 @@ echo "Switching HEAD to master"
 # we want the head to be on the master branch
 git checkout master
 
-## Add the SSH keys stored in the variables to the agent store
-## We're using tr to fix line endings which makes ed25519 keys work
-## without extra base64 encoding.
-## https://gitlab.com/gitlab-examples/ssh-private-key/issues/1#note_48526556
-echo "$SSH_GITHUB_PUSH_SYNC" | tr -d '\r' | ssh-add - > /dev/null
-
 # now add the remotes for the GitHub repo using SSH
 git remote add github git@github.com:Skeletonxf/totally-automated-a11y-scanner.git
 # Push onto GitHub
